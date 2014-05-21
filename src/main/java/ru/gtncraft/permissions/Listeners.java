dab7ed0cb11f90c2ac06283d8bd102234ffd0ce3
maximvarentsov <admin@phpcode.us>
2014-05-21 17:06:12 +0400

    Remove permissions.build.

--- src/main/java/ru/gtncraft/permissions/Listeners.java
+++ src/main/java/ru/gtncraft/permissions/Listeners.java
@@ -2,14 +2,13 @@ package ru.gtncraft.permissions;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 /**
  * Listen for player-based events to keep track of players and build permissions.
@@ -50,44 +49,5 @@ class Listeners implements Listener {
     @SuppressWarnings("unused")
     public void onPlayerQuit(final PlayerQuitEvent event) {
         plugin.getManager().unregisterPlayer(event.getPlayer());
-    }
-
-    // Prevent doing things in the event of permissions.build: false
-
-    @EventHandler(ignoreCancelled = true)
-    @SuppressWarnings("unused")
-    public void onPlayerInteract(final PlayerInteractEvent event) {
-        if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_AIR) {
-            return;
-        }
-        if (!event.getPlayer().hasPermission("permissions.build")) {
-            bother(event.getPlayer());
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    @SuppressWarnings("unused")
-    public void onBlockPlace(final BlockPlaceEvent event) {
-        if (!event.getPlayer().hasPermission("permissions.build")) {
-            bother(event.getPlayer());
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    @SuppressWarnings("unused")
-    public void onBlockBreak(final BlockBreakEvent event) {
-        if (!event.getPlayer().hasPermission("permissions.build")) {
-            bother(event.getPlayer());
-            event.setCancelled(true);
-        }
-    }
-
-    void bother(final Player player) {
-        if (plugin.getConfig().getString("messages/build", "").length() > 0) {
-            String message = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages/build", ""));
-            player.sendMessage(message);
-        }
     }
 }
